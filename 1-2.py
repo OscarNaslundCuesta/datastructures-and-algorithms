7d28b4528eeee09cda2b2c12b49338a73f2ef260
OscarNaslundCuesta <81713281+OscarNaslundCuesta@users.noreply.github.com>
2023-05-18 15:38:24 +0200

    Initial commit

---
+++ 1-2.py
@@ -0,0 +1,14 @@
+# Write a function called "show_excitement" where the string
+# "I am super excited for this course!" is returned exactly
+# 5 times, where each sentence is separated by a single space.
+# Return the string with "return".
+# You can only have the string once in your code.
+# Don't just copy/paste it 5 times into a single variable!
+
+
+def show_excitement():
+    string = "I am super excited for this course! "
+    full_string = 5 * string
+    return full_string
+
+print show_excitement()
---
+++ linked_list_practice.py
@@ -0,0 +1,117 @@
+"""The LinkedList code from before is provided below.
+Add three functions to the LinkedList.
+"get_position" returns the element at a certain position.
+The "insert" function will add an element to a particular
+spot in the list.
+"delete" will delete the first element with that
+particular value.
+Then, use "Test Run" and "Submit" to run the test cases
+at the bottom."""
+
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+
+    def get_position(self, position):
+        """Get an element from a particular position.
+        Assume the first position is "1".
+        Return "None" if position is not in the list."""
+        current = self.head
+        n = 1
+        while n != position:
+            current = current.next
+            n += 1
+        if n == position:
+            return current
+        else:
+            return None
+
+
+    def insert(self, new_element, position):
+        """Insert a new node at the given position.
+        Assume the first position is "1".
+        Inserting at position 3 means between
+        the 2nd and 3rd elements."""
+
+        current = self.head             #initiate
+        n = 1
+
+        if position > 1:
+            while n+1 < position:       # go to position
+                current = current.next
+                n += 1
+
+            new_element.next = current.next # inserts new element
+            current.next = new_element
+
+        elif position == 1:             # if inserting at the beginning
+            new_element.next = self.head
+            self.head = new_element
+
+        pass
+
+
+    def delete(self, value):
+        """Delete the first node with a given value."""
+        current = self.head
+        previous = None
+
+        if current.value == value:
+            self.head = current.next
+            return
+
+        while current.value != value:
+            if current is None:
+                return              # returns if value is not found
+            previous = current
+            current = current.next
+        previous.next = current.next
+
+        pass
+
+# Test cases
+# Set up some Elements
+e1 = Element(1)
+e2 = Element(2)
+e3 = Element(3)
+e4 = Element(4)
+
+# Start setting up a LinkedList
+ll = LinkedList(e1)
+ll.append(e2)
+ll.append(e3)
+
+# Test get_position
+# Should print 3
+print ll.head.next.next.value
+# Should also print 3
+print ll.get_position(3).value
+
+# Test insert
+ll.insert(e4,3)
+# Should print 4 now
+print ll.get_position(3).value
+
+# Test delete
+ll.delete(1)
+# Should print 2 now
+print ll.get_position(1).value
+# Should print 4 now
+print ll.get_position(2).value
+# Should print 3 now
+print ll.get_position(3).value
