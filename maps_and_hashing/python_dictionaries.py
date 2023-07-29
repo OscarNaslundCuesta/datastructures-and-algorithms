130178c5139a5dcac33c1596a77565534c1d365e
OscarNaslundCuesta <81713281+OscarNaslundCuesta@users.noreply.github.com>
2023-07-30 00:00:07 +0200

    Create python_dictionaries.py

---
+++ maps_and_hashing/python_dictionaries.py
@@ -0,0 +1,53 @@
+"""Time to play with Python dictionaries!
+You're going to work on a dictionary that
+stores cities by country and continent.
+One is done for you - the city of Mountain
+View is in the USA, which is in North America.
+
+You need to add the cities listed below by
+modifying the structure.
+Then, you should print out the values specified
+by looking them up in the structure.
+
+Cities to add:
+Bangalore (India, Asia)
+Atlanta (USA, North America)
+Cairo (Egypt, Africa)
+Shanghai (China, Asia)"""
+
+locations = {'North America': {'USA': ['Mountain View']}}
+locations['Asia'] = {'India': ['Bangalore'], 'China': ['Shanghai']}
+locations['North America']['USA'].append('Atlanta')
+locations['Africa'] = {'Egypt': ['Cairo']}
+
+"""Print the following (using "print").
+1. A list of all cities in the USA in
+alphabetic order.
+2. All cities in Asia, in alphabetic
+order, next to the name of the country.
+In your output, label each answer with a number
+so it looks like this:
+1
+American City
+American City
+2
+Asian City - Country
+Asian City - Country"""
+
+print(1)
+usa_cities = locations['North America']['USA']
+usa_cities.sort()
+
+for city in usa_cities:
+    print(city)
+
+print(2)
+asian_cities = []
+for country, cities in locations['Asia'].items():
+    for city in cities:
+        city_country = "{} - {}".format(city, country)
+        asian_cities.append(city_country)
+asian_cities.sort()
+for city in asian_cities:
+    print(city)
+
